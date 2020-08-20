bae7fa4b76140721c856380f595ccf61be9696a8
Alan Bandeira <alan.p.bandeira@gmail.com>
2020-08-20 11:49:55 -0300

    removing firebase info, data will be stored in mongo using mongoengine

--- api/src/sos_data_api/app/__init__.py
+++ api/src/sos_data_api/app/__init__.py
@@ -1,10 +1,6 @@
-# import firebase_admin
 
-# from firebase_admin import credentials
 from flask import Flask
 from flask_restful import Api, Resource
-# cred = credentials.Certificate("path/to/serviceAccountKey.json")
-# firebase_admin.initialize_app(cred)
 
 
 def create_app(config_name: str):
